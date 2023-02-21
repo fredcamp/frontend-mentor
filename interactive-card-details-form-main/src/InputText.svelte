5d4f4e9ebfd8f0566f9021e93bb7dba305f8dff4
Fred Campo <rickalburo@gmail.com>
2023-02-22 00:58:01 +0800

    created input text component

---
+++ interactive-card-details-form-main/src/InputText.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  export let id: string = null
+  export let value: unknown
+  export let error: string
+</script>
+
+<input
+  type="text"
+  {id}
+  class="bg-clip-paddbox focus:bg-custom-gradient w-full rounded-lg border-2 bg-origin-border p-2 tracking-wider focus:border-transparent focus:outline-none"
+  autocomplete="no"
+  class:border-red={error}
+  class:border-gray={!error}
+  {...$$restProps}
+  bind:value
+/>
