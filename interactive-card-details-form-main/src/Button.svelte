cf81d1dd2781088a1af66ba2e16f45daf9f9e10d
Fred Campo <rickalburo@gmail.com>
2023-02-22 00:59:02 +0800

    created button component

---
+++ interactive-card-details-form-main/src/Button.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  let className: string
+  export { className as class }
+  export let type: 'button' | 'submit' = 'button'
+</script>
+
+<button {type} class={className} {...$$restProps} on:click>
+  <slot />
+</button>
