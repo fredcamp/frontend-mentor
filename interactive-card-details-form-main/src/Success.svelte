ecb031c32fa4bc560e83ceb49eccb9b931f03e49
Fred Campo <rickalburo@gmail.com>
2023-02-22 00:57:46 +0800

    created success component

---
+++ interactive-card-details-form-main/src/Success.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { blur } from 'svelte/transition'
+  import completeIcon from './assets/icon-complete.svg'
+  import Button from './Button.svelte'
+</script>
+
+<section
+  class="mx-auto mt-20 max-w-md p-5 lg:mt-0 lg:place-self-center lg:py-0"
+  in:blur
+>
+  <div class="flex flex-col items-center justify-center space-y-6">
+    <img src={completeIcon} alt="success" />
+    <h2 class="text-3xl uppercase tracking-widest">thank you!</h2>
+    <p class="text-gray-dark">We've added your card details</p>
+    <Button
+      class="bg-violet hover:bg-violet/90 focus:ring-violet !mt-7 w-full rounded-md py-3 text-center text-white focus:outline-none focus:ring-2 focus:ring-offset-2"
+      on:click>Continue</Button
+    >
+  </div>
+</section>
