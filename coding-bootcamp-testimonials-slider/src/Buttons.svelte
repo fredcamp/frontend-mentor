c02c23e0d22a3cf42ce5f84a9aeff0491fb3780f
Fred Campo <rickalburo@gmail.com>
2023-02-27 00:33:47 +0800

    added hovering effect for btns

--- coding-bootcamp-testimonials-slider/src/Buttons.svelte
+++ coding-bootcamp-testimonials-slider/src/Buttons.svelte
@@ -11,12 +11,12 @@
 </script>
 
 <div
-  class="flex items-center justify-between gap-2 rounded-full bg-white px-2 py-3 shadow-lg {className}"
+  class="flex select-none items-center justify-between gap-2 rounded-full bg-white px-2 py-3 shadow-lg {className}"
 >
-  <button class="px-2" on:click={() => updateCurrent('decrement')}>
-    <img src={prev} alt="previous" class="h-3 w-2" />
+  <button class="group px-2" on:click={() => updateCurrent('decrement')}>
+    <img src={prev} alt="previous" class="h-3 w-2 group-hover:saturate-[50]" />
   </button>
-  <button class="px-2" on:click={() => updateCurrent('increment')}>
-    <img src={next} alt="next" class="h-3 w-2" />
+  <button class="group px-2" on:click={() => updateCurrent('increment')}>
+    <img src={next} alt="next" class="h-3 w-2 group-hover:saturate-[50]" />
   </button>
 </div>
