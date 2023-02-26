a1f4b262f2bd52829af168502261b95e14198662
Fred Campo <rickalburo@gmail.com>
2023-02-26 21:50:04 +0800

    update tailwind config

--- coding-bootcamp-testimonials-slider/tailwind.config.cjs
+++ coding-bootcamp-testimonials-slider/tailwind.config.cjs
@@ -1,7 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
-  content: ['./index.html','./src/**/*.{svelte,ts,js}'],
+  content: ['./index.html', './src/**/*.{svelte,ts,js}'],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      currentColor: 'currentColor',
+      white: colors.white,
+      gray: 'hsl(240, 18%, 77%)',
+      blue: 'hsl(240, 38%, 20%)',
+    },
+    fontFamily: {
+      sans: ['Inter', ...defaultTheme.fontFamily.sans],
+    },
     extend: {},
   },
   plugins: [],
