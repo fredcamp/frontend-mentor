43f5d897bec43e5775de7c105c5f9a698f547e41
Fred Campo <rickalburo@gmail.com>
2023-03-01 17:05:39 +0800

    created new section card component

---
+++ news-homepage-main/src/components/NewArticle.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let title: string
+  export let body: string
+</script>
+
+<article class="my-7 space-y-2">
+  <h3 class="hover:text-orange text-xl font-bold text-white transition-colors">
+    <a href="/" class="block">{title}</a>
+  </h3>
+  <p class="text-gray leading-7">{body}</p>
+</article>
