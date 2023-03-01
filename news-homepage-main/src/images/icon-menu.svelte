08ba1dc2c2df26e76e12fabfa20de32faa2902d8
Fred Campo <rickalburo@gmail.com>
2023-03-01 17:04:45 +0800

    added image assets

---
+++ news-homepage-main/src/images/icon-menu.svelte
@@ -0,0 +1,7 @@
+<svg width="40" height="17" xmlns="http://www.w3.org/2000/svg"
+  ><g fill="currentColor" fill-rule="evenodd"
+    ><path d="M0 0h40v3H0zM0 7h40v3H0zM0 14h40v3H0z" /><path
+      d="M0 0h40v3H0z"
+    /></g
+  ></svg
+>
