8283080979e94174cf181b398dbe355cd114b07c
Fred Campo <rickalburo@gmail.com>
2023-03-01 17:07:03 +0800

    created nav large component

---
+++ news-homepage-main/src/components/Nav/NavLarge.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import Logo from '../../images/logo.svelte'
+  import NavLinks from './NavLinks.svelte'
+</script>
+
+<nav class="mx-auto max-w-screen-xl">
+  <div class="flex items-center justify-between px-6 py-9">
+    <h1>
+      <span class="sr-only">brand logo</span>
+      <Logo />
+    </h1>
+    <NavLinks class="flex items-center justify-between gap-10" />
+  </div>
+</nav>
