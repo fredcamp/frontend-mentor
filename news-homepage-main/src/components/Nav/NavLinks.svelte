f47d214fbbf2fef6cffd3eaf7cfd46a7731cfd21
Fred Campo <rickalburo@gmail.com>
2023-03-01 17:06:44 +0800

    created nav links component

---
+++ news-homepage-main/src/components/Nav/NavLinks.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  let className: string = ''
+  const links = ['Home', 'New', 'Popular', 'Trending', 'Categories']
+
+  export { className as class }
+</script>
+
+<ul class={className} {...$$restProps}>
+  {#each links as link}
+    <li class="hover:text-orange select-none transition-colors">
+      <a href="/" class="block">{link}</a>
+    </li>
+  {/each}
+</ul>
