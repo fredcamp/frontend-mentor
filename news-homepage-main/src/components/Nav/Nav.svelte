334b7a349a92ee67198992225f0fe0b617dbeaa7
Fred Campo <rickalburo@gmail.com>
2023-03-01 17:07:32 +0800

    render lg and sm nav

---
+++ news-homepage-main/src/components/Nav/Nav.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import NavSmall from './NavSmall.svelte'
+  import NavLarge from './NavLarge.svelte'
+
+  let innerWidth = window.innerWidth
+</script>
+
+<svelte:window bind:innerWidth />
+
+{#if innerWidth > 670}
+  <NavLarge />
+{:else}
+  <NavSmall />
+{/if}
