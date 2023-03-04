bb60b77838ae4387916b98455e38e8e10eb72f63
Fred Campo <rickalburo@gmail.com>
2023-03-04 20:03:47 +0800

    created nav components and render sm and lg navs

---
+++ intro-section-with-dropdown-navigation-main/src/components/Nav.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import NavSmall from './NavSmall.svelte'
+  import NavLarge from './NavLarge.svelte'
+
+  let innerWidth = window.innerWidth
+</script>
+
+<svelte:window bind:innerWidth />
+
+<nav>
+  {#if innerWidth > 1023}
+    <NavLarge />
+  {:else}
+    <NavSmall />
+  {/if}
+</nav>
