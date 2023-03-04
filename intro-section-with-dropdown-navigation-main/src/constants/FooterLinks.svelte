d4ed20bf27268f1385c6bb3a6a10a3a2a5664625
Fred Campo <rickalburo@gmail.com>
2023-03-04 20:02:52 +0800

    created footer links constants

---
+++ intro-section-with-dropdown-navigation-main/src/constants/FooterLinks.svelte
@@ -0,0 +1,27 @@
+<script lang="ts" context="module">
+  import DataBizIcon from '../icons/client-databiz.svelte'
+  import AudioPhileIcon from '../icons/client-audiophile.svelte'
+  import MeetIcon from '../icons/client-meet.svelte'
+  import MakerIcon from '../icons/client-maker.svelte'
+
+  export function createFooterLinks() {
+    return [
+      {
+        name: 'Databiz',
+        Icon: DataBizIcon,
+      },
+      {
+        name: 'Audio Phile',
+        Icon: AudioPhileIcon,
+      },
+      {
+        name: 'Meet',
+        Icon: MeetIcon,
+      },
+      {
+        name: 'Maker',
+        Icon: MakerIcon,
+      },
+    ]
+  }
+</script>
