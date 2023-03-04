dcdb76c4856a98e09b31257265e0f4a9deb05aa8
Fred Campo <rickalburo@gmail.com>
2023-03-04 20:04:04 +0800

    created anchor link component

---
+++ intro-section-with-dropdown-navigation-main/src/components/Anchor.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  let className: string = ''
+
+  export { className as class }
+
+  function onClick(e): void {
+    e.preventDefault()
+    return
+  }
+</script>
+
+<a href="/" class={className} {...$$restProps} on:click={onClick}>
+  <slot />
+</a>
