a93b6fc44393865739954cc9a255bc7060f1f377
Fred Campo <rickalburo@gmail.com>
2023-03-04 20:00:40 +0800

    initial commit

---
+++ intro-section-with-dropdown-navigation-main/tailwind.config.cjs
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
+const plugin = require('tailwindcss/plugin')
+
+module.exports = {
+  content: ['./index.html', './src/**/*.{svelte,ts}'],
+  theme: {
+    colors: {
+      currentColor: colors.currentColor,
+      transparent: colors.transparent,
+      white: 'hsl(0, 0%, 98%)',
+      gray: 'hsl(0, 0%, 41%)',
+      black: 'hsl(0, 0%, 8%)',
+      slate: '#ADADAD',
+      indigo: colors.indigo,
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Epilogue', ...defaultTheme.fontFamily.sans],
+      },
+      boxShadow: {
+        custom: `0 0 10px rgba(0,0,0,0.5)`,
+      },
+    },
+  },
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        '.rotate-x-180': {
+          transform: 'rotateX(180deg)',
+        },
+      })
+    }),
+  ],
+}
