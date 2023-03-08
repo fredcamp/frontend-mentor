3b67804e3b0d2559f01a6936a6fbc8e0308d9c87
Fred Campo <rickalburo@gmail.com>
2023-03-08 14:08:02 +0800

    created button component

---
+++ fylo-dark-theme-landing-page-master/src/components/Button.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  export let type: 'submit' | 'button' = 'button'
+  let className: string = null
+  export { className as class }
+</script>
+
+<button {type} class="font-serif {className}" {...$$restProps} on:click>
+  <slot />
+</button>
