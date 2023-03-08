af0c5aeb1857758c14a1d811ebc0f4db3e6b0e83
Fred Campo <rickalburo@gmail.com>
2023-03-08 14:25:18 +0800

    import and render components

--- fylo-dark-theme-landing-page-master/src/App.svelte
+++ fylo-dark-theme-landing-page-master/src/App.svelte
@@ -1,46 +1,27 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  import favicon from './images/favicon-32x32.png'
+  import Nav from './components/Nav.svelte'
+  import Header from './components/Header.svelte'
+  import Features from './components/Features.svelte'
+  import Team from './components/Team.svelte'
+  import Testimonials from './components/Testimonials.svelte'
+  import SignIn from './components/SignIn.svelte'
+  import Footer from './components/Footer.svelte'
+
+  import { applyTheme } from './utils/theme'
+  applyTheme()
 </script>
 
+<svelte:head>
+  <link rel="shortcut icon" href={favicon} type="image/x-icon" />
+</svelte:head>
+
+<Nav />
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <Header />
+  <Features />
+  <Team />
+  <Testimonials />
+  <SignIn />
 </main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+<Footer />
