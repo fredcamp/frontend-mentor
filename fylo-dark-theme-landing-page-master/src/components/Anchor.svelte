7c87c69e515aa8109e1e9fb899fd9f94902d349a
Fred Campo <rickalburo@gmail.com>
2023-03-08 14:07:40 +0800

    created anchor component

---
+++ fylo-dark-theme-landing-page-master/src/components/Anchor.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  let className: string = null
+
+  export { className as class }
+  export let url: string = '/'
+</script>
+
+<a href={url} class="font-serif {className}" {...$$restProps} on:click>
+  <slot />
+</a>
