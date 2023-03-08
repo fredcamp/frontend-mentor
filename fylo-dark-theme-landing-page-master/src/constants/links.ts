becd9c1a18aa0d380aba3066f08d7640402e5ff6
Fred Campo <rickalburo@gmail.com>
2023-03-08 14:06:12 +0800

    created main and footer links

---
+++ fylo-dark-theme-landing-page-master/src/constants/links.ts
@@ -0,0 +1,40 @@
+const mainNav = [
+  {
+    url: '#features',
+    title: 'Features',
+  },
+  {
+    url: '#team',
+    title: 'Team',
+  },
+  {
+    url: '#sign-in',
+    title: 'Sign In',
+  },
+]
+
+const footerNav = [
+  {
+    title: 'About Us',
+  },
+  {
+    title: 'Jobs',
+  },
+  {
+    title: 'Press',
+  },
+  {
+    title: 'Blog',
+  },
+  {
+    title: 'Contact Us',
+  },
+  {
+    title: 'Terms',
+  },
+  {
+    title: 'Privacy',
+  },
+]
+
+export { mainNav, footerNav }
