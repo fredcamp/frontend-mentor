b56d08e633f181d2dd9c81bbaba92608e472e9d6
Fred Campo <rickalburo@gmail.com>
2023-03-09 09:11:21 +0800

    initial commit

---
+++ static-job-listings-master/tailwind.config.cjs
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: ['./index.html', './src/**/*.{svelte,ts}'],
+  theme: {
+    colors: {
+      currentColor: colors.currentColor,
+      transparent: colors.transparent,
+      cyan: {
+        primary: 'hsl(180, 29%, 50%)',
+        'gray-light-bg': 'hsl(180, 52%, 96%)',
+        'gray-light-tab': 'hsl(180, 31%, 95%)',
+        'dark-gray': 'hsl(180, 8%, 52%)',
+        'darker-gray': 'hsl(180, 14%, 20%)',
+      },
+    },
+    extend: {
+      fontFamily: {
+        sans: ['League Spartan', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+}
