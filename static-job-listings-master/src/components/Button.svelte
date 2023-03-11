e94b1e124fa0eeb61d9bfdb714b3069f8ffac762
Fred Campo <rickalburo@gmail.com>
2023-03-11 14:06:22 +0800

    created button component

---
+++ static-job-listings-master/src/components/Button.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  export let type: 'button' | 'submit' = 'button'
+  let className: string = null
+
+  export { className as class }
+</script>
+
+<button {type} class="select-none {className}" {...$$restProps} on:click>
+  <slot />
+</button>
