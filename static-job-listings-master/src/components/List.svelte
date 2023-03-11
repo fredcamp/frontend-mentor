b0623161f48b39b920fe9e3fa525bcec5ce425d6
Fred Campo <rickalburo@gmail.com>
2023-03-11 14:06:56 +0800

    created job lsit component

---
+++ static-job-listings-master/src/components/List.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { fade } from 'svelte/transition'
+  import Card from './Card.svelte'
+  import type { Job } from '../types/Job.type'
+
+  export let data: Job[] = []
+</script>
+
+<section class="mt-14">
+  <ul class="space-y-16 xl:space-y-10">
+    {#each data as job (job.id)}
+      <li out:fade>
+        <Card {...job} />
+      </li>
+    {/each}
+  </ul>
+</section>
