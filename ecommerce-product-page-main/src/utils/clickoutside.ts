4f7fd63b33ec12d28072ce8d9f6716970074b8c1
Fred Campo <rickalburo@gmail.com>
2023-03-15 17:02:01 +0800

    create click outside action func

---
+++ ecommerce-product-page-main/src/utils/clickoutside.ts
@@ -0,0 +1,15 @@
+export default function clickOutside(node: HTMLElement) {
+  function onClick(e) {
+    if (node.contains(e.target)) return
+
+    node.dispatchEvent(new CustomEvent('outclick'))
+  }
+
+  window.addEventListener('click', onClick, true)
+
+  return {
+    destroy() {
+      window.removeEventListener('click', onClick, true)
+    },
+  }
+}
