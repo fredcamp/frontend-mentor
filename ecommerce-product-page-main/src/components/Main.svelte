f58a7ddaef80be2c6bdd7a3b211c15cde9ad3a3d
Fred Campo <rickalburo@gmail.com>
2023-03-15 17:01:41 +0800

    create main component

---
+++ ecommerce-product-page-main/src/components/Main.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import isDesktop from '../stores/viewport'
+
+  let className = null
+
+  $: if ($isDesktop) {
+    className =
+      'max-w-screen-xl mx-auto px-8 grid grid-cols-2 items-center justify-between gap-x-10 my-20'
+  } else {
+    className = null
+  }
+</script>
+
+<main class={className}>
+  <slot />
+</main>
