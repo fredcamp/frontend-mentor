157d4a3f1301fcd333ca375c1b704eb4554d53bd
Fred Campo <rickalburo@gmail.com>
2023-03-15 16:59:58 +0800

    update tailwind theme

--- ecommerce-product-page-main/tailwind.config.cjs
+++ ecommerce-product-page-main/tailwind.config.cjs
@@ -1,9 +1,31 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: ['./index.html', './src/**/*.{ts,svelte}'],
   theme: {
-    extend: {},
+    colors: {
+      transparent: colors.transparent,
+      currentColor: colors.currentColor,
+      orange: {
+        DEFAULT: 'hsl(26, 100%, 55%)',
+        pale: 'hsl(25, 100%, 94%)',
+      },
+      white: 'hsl(0, 0%, 100%)',
+      black: 'hsl(0, 0%, 0%)',
+      blue: {
+        DEFAULT: 'hsl(220, 13%, 13%)',
+        'gray-dark': 'hsl(219, 9%, 45%)',
+        gray: 'hsl(220, 14%, 75%)',
+        light: 'hsl(223, 64%, 98%)',
+      },
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Kumbh Sans', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }
