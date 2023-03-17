22d1712f9453a55c92a3cce5db7141fd4717a615
Fred Campo <rickalburo@gmail.com>
2023-03-17 17:34:11 +0800

    added navlinks styling for lg and sm

--- ecommerce-product-page-main/src/components/NavLinks.svelte
+++ ecommerce-product-page-main/src/components/NavLinks.svelte
@@ -1,12 +1,20 @@
 <script lang="ts">
+  import isDesktop from '../stores/viewport'
+
   let className: string = null
 
   export { className as class }
   const links = ['Collections', 'Men', 'Women', 'About', 'Contact']
+
+  const anchorClass = $isDesktop
+    ? 'relative transition-colors after:absolute after:bottom-0 after:left-0 after:right-0 after:h-[3px] px-1 after:origin-right after:translate-y-8 after:scale-x-0 after:bg-orange after:transition-transform hover:text-black after:hover:origin-left hover:after:scale-x-100'
+    : 'transition-colors hover:text-black'
 </script>
 
 <ul class={className}>
   {#each links as link}
-    <li><a href="/" class="transition-colors hover:text-black">{link}</a></li>
+    <li>
+      <a href="/" class={anchorClass}>{link}</a>
+    </li>
   {/each}
 </ul>
