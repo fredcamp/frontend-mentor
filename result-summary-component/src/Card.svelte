1e6f7fd86e9c4e6358cfb811f1792250308f95b3
Fred Campo <rickalburo@gmail.com>
2023-07-01 18:38:18 +0800

    created score card component

---
+++ result-summary-component/src/Card.svelte
@@ -0,0 +1,22 @@
+<script>
+  export let icon
+  export let category
+  export let score
+  export let total
+
+  let className
+
+  export { className as class }
+</script>
+
+<div
+  class="{className} flex flex-wrap items-center justify-between rounded-md px-6 py-4 font-bold md:text-base"
+>
+  <div class="flex space-x-3">
+    <img src={icon} alt={category} />
+    <h3>{category}</h3>
+  </div>
+  <p class="text-gray/50 font-extrabold">
+    <span class="text-gray">{score}</span> / {total}
+  </p>
+</div>
