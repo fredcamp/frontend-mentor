c9a258af288fd4499be76f06d6568561384db190
Fred Campo <rickalburo@gmail.com>
2023-07-01 18:37:24 +0800

    added tailwind config and theme

--- result-summary-component/tailwind.config.js
+++ result-summary-component/tailwind.config.js
@@ -1,8 +1,34 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 export default {
   content: ['./index.html', './src/**/*.{js,svelte}'],
   theme: {
-    extend: {},
+    colors: {
+      currentColor: colors.currentColor,
+      transparent: colors.transparent,
+      red: 'hsl(0, 100%, 67%)',
+      yellow: 'hsl(39, 100%, 56%)',
+      green: 'hsl(166, 100%, 37%)',
+      blue: {
+        DEFAULT: 'hsl(234, 85%, 45%)',
+        slate: 'hsl(252, 100%, 67%)',
+        royal: 'hsl(241, 81%, 54%)',
+        violet: 'hsla(256, 72%, 46%, 1)',
+        persian: 'hsla(241, 72%, 46%, 0)',
+      },
+      white: 'hsl(0, 0%, 100%)',
+      pale: 'hsl(221, 100%, 96%)',
+      lavender: 'hsl(241, 100%, 89%)',
+      gray: 'hsl(224, 30%, 27%)',
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Hanken Grotesk', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }
