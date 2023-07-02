5540e2d99dee761a16b68128f80fc9f405348782
Fred Campo <rickalburo@gmail.com>
2023-07-03 07:58:44 +0800

    created button component

---
+++ sunny-side-agency-page/src/components/Button.svelte
@@ -0,0 +1,12 @@
+<script>
+  // @ts-nocheck
+
+  export let type = 'button'
+  let className = null
+
+  export { className as class }
+</script>
+
+<button {type} class={className} {...$$restProps} on:click>
+  <slot />
+</button>
