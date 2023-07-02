4e81cd92450084366bb074e111c289931b7bd3d6
Fred Campo <rickalburo@gmail.com>
2023-07-03 07:59:53 +0800

    created main nav component

---
+++ sunny-side-agency-page/src/components/Nav.svelte
@@ -0,0 +1,14 @@
+<script>
+  import NavMobile from './NavMobile.svelte'
+  import NavLarge from './NavLarge.svelte'
+
+  let innerWidth = window.innerWidth
+</script>
+
+<svelte:window bind:innerWidth />
+
+{#if innerWidth > 760}
+  <NavLarge />
+{:else}
+  <NavMobile />
+{/if}
