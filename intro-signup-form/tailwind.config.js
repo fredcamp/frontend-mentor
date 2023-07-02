7354b821900561c83435e57df6665c2e34b4f556
Fred Campo <rickalburo@gmail.com>
2023-07-02 14:13:27 +0800

    added tailwind theme and config

--- intro-signup-form/tailwind.config.js
+++ intro-signup-form/tailwind.config.js
@@ -1,9 +1,37 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 export default {
-  content: [],
+  content: ['./index.html', './src/**/*.{js,svelte}'],
   theme: {
-    extend: {},
+    colors: {
+      currentColor: colors.currentColor,
+      transparent: colors.transparent,
+      white: colors.white,
+      black: colors.black,
+      red: 'hsl(0, 100%, 74%)',
+      green: 'hsl(154, 59%, 51%)',
+      blue: {
+        DEFAULT: 'hsl(248, 32%, 49%)',
+        dark: 'hsl(249, 10%, 26%)',
+        gray: 'hsl(246, 25%, 77%)',
+      },
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Poppins', ...defaultTheme.fontFamily.sans],
+      },
+      backgroundImage: {
+        'pattern-desktop': 'url("/images/bg-intro-desktop.png")',
+        'pattern-mobile': 'url("/images/bg-intro-mobile.png")',
+      },
+      boxShadow: {
+        under: '0 8px rgba(0, 0, 0, 0.2)',
+        inset: 'inset 0 -4px rgba(0, 0, 0, 0.2)',
+      },
+    },
   },
   plugins: [],
 }
-
