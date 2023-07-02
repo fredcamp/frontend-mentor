0f5921a76992c65df0c97e812207edfe98516d3a
Fred Campo <rickalburo@gmail.com>
2023-07-03 07:58:08 +0800

    created outclick action

---
+++ sunny-side-agency-page/src/utils/clickOutside.js
@@ -0,0 +1,15 @@
+export default function (node) {
+  function onClick(e) {
+    if (node.contains(e.target)) return
+
+    node.dispatchEvent(new CustomEvent('outclick'))
+  }
+
+  document.body.addEventListener('click', onClick, true)
+
+  return {
+    destroy() {
+      document.body.removeEventListener('click', onClick, true)
+    },
+  }
+}
