e2025dd8fefd07f248ed2d5fc9d8c9f957cb71de
Fred Campo <rickalburo@gmail.com>
2023-07-03 08:00:06 +0800

    created services component

---
+++ sunny-side-agency-page/src/components/Services.svelte
@@ -0,0 +1,48 @@
+<section id="Services">
+  <div class="lg:grid lg:grid-cols-2">
+    <article class="relative">
+      <picture>
+        <source
+          media="(min-width: 1024px)"
+          srcset="/images/desktop/image-graphic-design.jpg"
+        />
+        <img
+          src="/images/mobile/image-graphic-design.jpg"
+          alt="closeup photography of cherry fruit"
+          class="h-full w-full object-cover object-top"
+        />
+      </picture>
+      <div
+        class="text-primary-cyan absolute bottom-0 left-1/2 w-full -translate-x-1/2 space-y-9 text-center"
+      >
+        <h2 class="font-serif text-4xl font-black">Graphic design</h2>
+        <p class="mx-auto max-w-md pb-14 text-xl lg:pb-8 xl:pb-14">
+          Great design makes you memorable. We deliver artwork that underscores
+          your brand message and captures potential clients’ attention.
+        </p>
+      </div>
+    </article>
+    <article class="relative">
+      <picture>
+        <source
+          media="(min-width: 1024px)"
+          srcset="/images/desktop/image-photography.jpg"
+        />
+        <img
+          src="/images/mobile/image-photography.jpg"
+          alt="closeup photography of orange fruit"
+          class="h-full w-full object-cover object-top"
+        />
+      </picture>
+      <div
+        class="text-primary-blue absolute bottom-0 left-1/2 w-full -translate-x-1/2 space-y-9 text-center"
+      >
+        <h2 class="font-serif text-4xl font-black">Photography</h2>
+        <p class="mx-auto max-w-md pb-14 text-xl lg:pb-8 xl:pb-14">
+          Increase your credibility by getting the most stunning, high-quality
+          photos that improve your business image.
+        </p>
+      </div>
+    </article>
+  </div>
+</section>
