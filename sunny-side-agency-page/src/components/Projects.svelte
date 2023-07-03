24c0489be4f0f288f290ba4ef77e31c59aa0e899
Fred Campo <rickalburo@gmail.com>
2023-07-03 08:00:00 +0800

    created projects component

---
+++ sunny-side-agency-page/src/components/Projects.svelte
@@ -0,0 +1,41 @@
+<script>
+  const projects = [
+    {
+      mobile: '/images/mobile/image-gallery-milkbottles.jpg',
+      desktop: '/images/desktop/image-gallery-milkbottles.jpg',
+      alt: 'milk bottles',
+    },
+    {
+      mobile: '/images/mobile/image-gallery-orange.jpg',
+      desktop: '/images/desktop/image-gallery-orange.jpg',
+      alt: 'sliced orange on plate',
+    },
+    {
+      mobile: '/images/mobile/image-gallery-cone.jpg',
+      desktop: '/images/desktop/image-gallery-cone.jpg',
+      alt: 'icecream cone',
+    },
+    {
+      mobile: '/images/mobile/image-gallery-sugar-cubes.jpg',
+      desktop: '/images/desktop/image-gallery-sugar-cubes.jpg',
+      alt: 'stacks of sugar cubes',
+    },
+  ]
+</script>
+
+<section id="Projects">
+  <ul class="grid grid-cols-2 lg:grid-cols-4">
+    {#each projects as project}
+      <li>
+        <picture>
+          <source media="(min-width: 1240px )" srcset={project.desktop} />
+          <img
+            src={project.mobile}
+            alt={project.alt}
+            class="h-full w-full cursor-pointer object-cover object-center transition-transform duration-300 hover:scale-105 hover:shadow-lg"
+          />
+        </picture>
+      </li>
+    {/each}
+  </ul>
+</section>
