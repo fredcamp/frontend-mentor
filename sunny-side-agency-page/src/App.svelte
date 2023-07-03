d63d2e448a5bde1d082632dd1d9f1fd991f77093
Fred Campo <rickalburo@gmail.com>
2023-07-03 08:00:21 +0800

    created main component

---
+++ sunny-side-agency-page/src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+  import Nav from './components/Nav.svelte'
+  import Home from './components/Home.svelte'
+  import About from './components/About.svelte'
+  import Services from './components/Services.svelte'
+  import Testimonials from './components/Testimonials.svelte'
+  import Projects from './components/Projects.svelte'
+  import Footer from './components/Footer.svelte'
+</script>
+
+<Nav />
+<main>
+  <Home />
+  <About />
+  <Services />
+  <Testimonials />
+  <Projects />
+  <Footer />
+</main>
